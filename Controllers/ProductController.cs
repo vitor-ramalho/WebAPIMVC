7ed18dc36930906a0e405e7ab62907f162c5e3d3
Vitor Ramalho <vitorrvilela@hotmail.com>
2020-10-25 13:19:21 -0300

    WEB API CRUD .NET

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,87 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using testeef.Data;
+using testeef.Models;
+
+namespace testeef.Controllers
+{
+    [ApiController]
+    [Route("v1/products")]
+
+    public class ProductController : ControllerBase
+    {
+        //Listar produtos
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Product>>> Get ([FromServices] DataContext context){
+
+            var products = await context.Products.Include(x => x.Category).ToListAsync();
+            return products;
+        }
+
+        //Listar produto
+        [HttpGet]
+        [Route("{id:int}")]
+
+        public async Task<ActionResult<Product>> GetById([FromServices] DataContext context, int id){
+
+            var product = await context.Products.Include(x => x.Category)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == id);
+            return product;
+        }
+
+        //Deletar produto
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Product>> DeleteProduct([FromServices] DataContext context, int id){
+            var product = await context.Products.FindAsync(id);
+
+            if(product == null){
+                return NotFound();
+            }
+
+            context.Products.Remove(product);
+            await context.SaveChangesAsync();
+
+            return product;
+        }
+
+        //Listar produto por categoria
+        [HttpGet]
+        [Route("categories/{id:int}")]
+        public async Task<ActionResult<List<Product>>> GetByCategory([FromServices] DataContext context, int id){
+
+            var products = await context.Products
+            .Include(x => x.Category)
+            .AsNoTracking()
+            .Where(x => x.CategoryId == id)
+            .ToListAsync();
+            return products;
+
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Product>> Post(
+            [FromServices] DataContext context,
+            [FromBody] Product model)
+            {
+                if (ModelState.IsValid)
+                {
+                    context.Products.Add(model);
+                    await context.SaveChangesAsync();
+                    return model;
+                }
+                else
+                {
+                    return BadRequest(ModelState);
+                }
+            }
+        
+
+    }
+}
